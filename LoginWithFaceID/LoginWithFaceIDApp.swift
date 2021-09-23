50d513124359b63838bfa7548c6024400c75edcd
sotomaque <sotomaque@gmail.com>
2021-09-22 20:52:16 -0700

    Initial Commit

---
+++ LoginWithFaceID/LoginWithFaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithFaceIDApp.swift
+//  LoginWithFaceID
+//
+//  Created by Enrique Sotomayor on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithFaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
